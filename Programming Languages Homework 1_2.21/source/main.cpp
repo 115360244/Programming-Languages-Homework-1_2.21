51a96672670573d8c728bfbb96778c7a92ec0a52
115360244 <t115360244@ntut.org.tw>
2026-09-21 21:57:53 +0800

    Add files via upload

---
+++ Programming Languages Homework 1_2.21/source/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "*********     ***       *        *    " << endl;
+    cout << "*       *   *     *    ***      * *   " << endl;
+    cout << "*       *  *       *  *****    *   *  " << endl;
+    cout << "*       *  *       *    *     *     * " << endl;
+    cout << "*       *  *       *    *    *       *" << endl;
+    cout << "*       *  *       *    *     *     * " << endl;
+    cout << "*       *  *       *    *      *   *  " << endl;
+    cout << "*       *   *     *     *       * *   " << endl;
+    cout << "*********     ***       *        *    " << endl;
+    system("pause");
+    return 0;
+}
